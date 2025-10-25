693a1ccd24438124cf62ff758d4e8a8d909dc38c
DOAN <vdoan309@gmail.com>
2025-10-25 22:37:12 +0700

    1st

---
+++ Models/AppDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogWebsite.Models
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>()
+                .HasIndex(c => c.Slug);
+
+        }
+    }
+}
